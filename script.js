4dfd0105797d1bdca2be5853747ff09fd12c5c3c
Eric-Obeng <obengeric.3958@gmail.com>
2024-06-23 12:35:06 +0000

    Methods and Functionalities for usePower(powerName) and revealIdentity completed also the superhero object have been updated

--- script.js
+++ script.js
@@ -4,8 +4,22 @@
 const superhero = {
   name: "Superman",
   secretIdentity: "Clark Kent",
-  power: ["Super strength", "Fight", "X-ray vision", "Heat vision"],
+  powers: ["Super strength", "Flight", "X-ray vision", "Heat vision"],
   weakness: "Kryptonite",
+  //   Method for power use
+  usePower(powerName) {
+    this.powers.includes(powerName)
+      ? console.log(`${this.name} uses ${powerName}`)
+      : console.log(`${this.name} does not have the power of ${powerName}`);
+  },
+
+  //   Method to reveal secret identity
+  revealIdentity() {
+    console.log(`${this.name}'s secret identity is ${this.secretIdentity}`);
+  },
 };
 
 console.log(superhero);
+superhero.usePower("Flight");
+superhero.usePower("Invisibility");
+superhero.revealIdentity();
