debc96df2ecf9843a83822639e148731d29884f8
Eric-Obeng <obengeric.3958@gmail.com>
2024-06-23 12:17:09 +0000

    Object representing superhero with properties

---
+++ script.js
@@ -0,0 +1,11 @@
+"use strict";
+
+// Object Creation Basics
+const superhero = {
+  name: "Superman",
+  secretIdentity: "Clark Kent",
+  power: ["Super strength", "Fight", "X-ray vision", "Heat vision"],
+  weakness: "Kryptonite",
+};
+
+console.log(superhero);
